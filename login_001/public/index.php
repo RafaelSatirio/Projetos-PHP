dd598f67245799ab414e13f597b877748c9e0d9f
RafaelSatirio <125366637+RafaelSatirio@users.noreply.github.com>
2024-09-04 18:09:19 -0300

    projeto login com php puro sem banco de dados

---
+++ login_001/public/index.php
@@ -0,0 +1,39 @@
+<?php
+
+// iniciar a sessão
+session_start();
+
+// definir uma constante de controle
+define('CONTROL', true);
+
+// verifica se existe um usuario logado 
+$usuario_logado = $_SESSION['usuario'] ?? null;
+
+// verifica qual é a rota na URL
+if(empty($usuario_logado)) {
+    $rota = 'login';
+} else {
+    $rota = $_GET['rota'] ?? 'home';
+}
+
+// se o usuário está logado, mas a rota é login, vai redirecionar para o home
+if(!empty($usuario_logado) && $rota == 'login'){
+    $rota = 'home';
+}
+
+// analisa a rota
+$rotas = [
+    'login' => 'login.php',
+    'home' => 'home.php',
+    'page1' => 'page1.php',
+    'page2' => 'page2.php',
+    'page3' => 'page3.php',
+    'logout' => 'logout.php'
+];
+
+if(!key_exists($rota, $rotas)){
+    die('Acesso negado!');
+} else {
+    require_once $rotas[$rota];
+}
+?>
